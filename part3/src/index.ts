46ed4662e118084b1eee956ebd679153d9e64b21
chuhelan <chuhelan@163.com>
2023-06-01 15:49:22 +0800

    :beer: try webpack, babel...

---
+++ part3/src/index.ts
@@ -0,0 +1,16 @@
+import {hi} from "./m1";
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+const obj = {name: '孙悟空', age: 33};
+
+console.log(obj);
+obj.age = 18;
+
+console.log(sum(123, 456));
+console.log("haha");
+console.log(hi)
+
+console.log(Promise);
